a161290030d00aa165e4959f2437701a9a86ce1c
marimonon <mappi-@hotmail.co.jp>
2023-11-05 22:44:02 +0900

    assetsの追加

---
+++ assets/js/common.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+
+  var toggleDrawer = function toggleDrawer() {
+    document.body.classList.toggle('is-drawer-active');
+  };
+  document.getElementById('drawer_btn').addEventListener('click', toggleDrawer);
+  document.getElementById('drawer_shade').addEventListener('click', toggleDrawer);
+
+})();
